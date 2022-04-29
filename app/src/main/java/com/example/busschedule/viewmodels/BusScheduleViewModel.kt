36d63cef46c8b54e42a60f4ce7150ac27a647748
skatingtamara <tamarabpatterson@gmail.com>
2022-04-28 20:34:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.busschedule.viewmodels
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.Flow
+import androidx.lifecycle.ViewModelProvider
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.database.schedule.ScheduleDao
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
+
+    fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
+}
+
+// Factory to intantiate view model objects
+
+class BusScheduleViewModelFactory(
+    private val scheduleDao: ScheduleDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
